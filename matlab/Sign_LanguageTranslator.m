5c6af31de3bc77d107ca9f692632ec77a836311e
benjaminSullins <bensullins1@gmail.com>
2016-12-02 01:04:19 -0700

    Added errro rate to GUI Reduced terminal output spewage Signed-off-by: benjaminSullins <bensullins1@gmail.com>

--- matlab/Sign_LanguageTranslator.m
+++ matlab/Sign_LanguageTranslator.m
@@ -71,7 +71,7 @@ if exist(fullfile(currentFolder, file), 'file') == 2
 
     aa= load('trainingFeatureVectorDefault.mat', 'CDefault');
     bb= load('trainingFeatureVectorDefault.mat', 'trainingFeatureVectorDefault');
-    handles.trainingFeatureVector = bb.trainingFeatureVectorDefault
+    handles.trainingFeatureVector = bb.trainingFeatureVectorDefault;
     C = aa.CDefault;
     
      handles.C = C;
@@ -135,6 +135,11 @@ handles=guidata(hObject);
 
 timeStart = tic;
 
+if ~exist('handles.error_rate')
+    handles.error_rate = 0;
+    handles.totalRuns = 0;
+end
+
 if handles.data2valid~=0 &&(handles.extracted == 1 ||handles.trainingdata ==1)
    
             disp('testing')
@@ -178,7 +183,7 @@ if handles.data2valid~=0 &&(handles.extracted == 1 ||handles.trainingdata ==1)
     [handles.label handles.corrPlot] = classify( numVectors, handles.trainingFeatureVector, handles.C, handles.thisx ); % , handles.to, handles.t1);
     
     % Update the Output Image
-    handles.img = getImage( '../imagesPristine/', handles.label );
+    [ handles.img handles.outputFile] = getImage( '../imagesPristine/', handles.label );
 
 %     axes(handles.axes7);
 %     imshow(handles.img);
@@ -200,6 +205,15 @@ if handles.data2valid~=0 &&(handles.extracted == 1 ||handles.trainingdata ==1)
 
 timeElapsed = toc(timeStart);
 
+% Check for error
+if handles.file ~= handles.outputFile
+    handles.error_rate  = handles.error_rate + 1;
+end
+
+handles.totalRuns   = handles.totalRuns + 1;
+error = (handles.error_rate / handles.totalRuns ) * 100;
+
+set(handles.textError, 'String', strcat( num2str(error), ' %') );
 set(handles.textSpeed, 'String', strcat( num2str(timeElapsed), ' seconds') );
 set(handles.text26, 'String', 'Complete!' );
     
